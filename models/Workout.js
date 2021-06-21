7424034d217fa32172de997a31b7a2f6701188fe
Brent B <brentbaughan1@gmail.com>
2021-06-21 13:52:34 -0400

    last workout display works as intended now via virtual totalDuration

--- models/Workout.js
+++ models/Workout.js
@@ -4,7 +4,15 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
     date: Date,
     exercises: Array
-}, { toJSON: { virtuals: true } }); //deploy virtual for summing up total workout duration
+}, { toJSON: { virtuals: true }, toObject: { virtuals: true } }); //deploy virtual for summing up total workout duration
+
+WorkoutSchema.virtual('totalDuration').get(function() { //must declare as standard function, rather than arrow function; must also not be async
+    let totalDuration = 0;
+    this.exercises.forEach((exercise) => {
+        totalDuration += exercise.duration; 
+    })
+    return totalDuration
+})
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 

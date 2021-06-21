ef5eacc37bd0295d893d0941b71a684d86bf5ee4
Brent B <brentbaughan1@gmail.com>
2021-06-21 15:13:15 -0400

    created render routes for other pages; workout PUT method functions

--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -9,5 +9,21 @@ router.get('/', async (req, res) => {
         res.status(500).json(err)
     }
 })
+router.post('/', async (req, res) => {
+    try {
+        const postResult = await db.Workout.create(req.body)
+        res.status(200).json(postResult)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
+router.put('/:id', async (req, res) =>{
+    try {
+        const putResult = await db.Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}})
+        res.status(200).json(putResult)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 module.exports = router;
